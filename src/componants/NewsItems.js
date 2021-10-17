3b96d3ef18de8431b166500eda6ec87746f5805a
Raj Mhatre <rajjimh18phpcomp@student.mes.ac.in>
2021-10-17 13:33:02 +0530

    Changes

--- src/componants/NewsItems.js
+++ src/componants/NewsItems.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItems extends Component {
-    
-    render() {
-        let {title, discription, imageUrl, newsUrl, author, date} = this.props;
-        return (
-            <div>
-                <div className="card" >
-                    <img src={!imageUrl?"https://s.w-x.co/in-galaxy_8.jpg":imageUrl} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{discription}...</p>
-                        <p className="card-text"><small className="text-muted">By {author} on {date}</small></p>
-                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    let { title, discription, imageUrl, newsUrl, author, date, source } = this.props;
+    return (
+      <div>
+        <div className="card">
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '85%', zIndex: '1'}}>
+            {source}
+          </span>
+          <img
+            src={!imageUrl ? "https://s.w-x.co/in-galaxy_8.jpg" : imageUrl}
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{discription}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewsItems 
+export default NewsItems;
