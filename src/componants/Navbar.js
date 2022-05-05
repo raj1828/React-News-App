0bb265f905e22b23c55982331f18e6e744ea54b9
Raj Mhatre <rajjimh18phpcomp@student.mes.ac.in>
2022-05-05 22:23:09 +0530

    Changes

--- src/componants/Navbar.js
+++ src/componants/Navbar.js
@@ -8,7 +8,7 @@ export class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="./index.html">News App</Link>
+                        <Link className="navbar-brand" to="./index.html">Newsify</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
